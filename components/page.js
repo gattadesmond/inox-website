372daaab248c496bd42a9367673d214f12d44e08
Soju Desmond <>
2021-03-12 16:18:09 +0700

    add slider

--- components/page.js
+++ components/page.js
@@ -1,12 +1,12 @@
 import PageContainer from './page-container';
 import Header from './header';
-import Hero from './hero';
+
 
 export default function Page({ title, description, sticky, children }) {
   return (
     <PageContainer title={title} description={description}>
       <Header sticky={sticky}  />
-      <Hero />
+      
       {children}
     </PageContainer>
   );
